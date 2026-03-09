447e75e0da90d515a38152a51d4c274b7496cd88
joaokanashiro-cmd <joao.kanashiro@escola.pr.gov.br>
2026-03-09 08:55:11 -0300

    Create cont03.py

---
+++ conteúdo/cont03.py
@@ -0,0 +1,10 @@
+n= input('digite algo')
+print(n.isnumeric())#para perguntar se é se um número
+print(n.isalpha())# para perguntar se é letra
+print(n.isalnum())#para perguntar se é letra ou número
+print(n.isupper())#para perguntar se é so com letra
+print(n.isdecimal())# para verificr se é decimal 
+print(n.isdigit())#para verificar se é um digito
+print(n.islower())#para verificar se é apenas letra minusculas
+print(n.isidentifier())#para verificar se é um identificador valido
+print(n.isspace())#para verificar se tem um espaço em branco
