e750751782f5fdd026326dd949deb8c26cbbe80e
joaokanashiro-cmd <joao.kanashiro@escola.pr.gov.br>
2026-03-11 11:35:16 -0300

    Create cont04.py

---
+++ conteúdo/cont04.py
@@ -0,0 +1,7 @@
+# o + é utilizado como operador de adição 
+# o - é utilizado como sinal de subração
+# o  * é utilizado para fazer a multiplicação
+# o / é utilizado para fazer divisão
+# o ** é utilizado para exponenciação 
+# // é utilizado para a divisão inteira
+# % é utilizado para o resto da divisão
