a21ffb7ac1ad5ad90c6ebb805f1cce588239034d
joaokanashiro-cmd <joao.kanashiro@escola.pr.gov.br>
2026-03-23 10:43:23 -0300

    Create ex12.py

---
+++ exercícios/ex12.py
@@ -0,0 +1,5 @@
+largura = float (input('largura da parede'))
+altura = float (input('altura da parede'))
+area = altura*largura 
+print ('sua parede tem a dimensão de {} x {} e sua área é de {}m quadrados ;'.format(largura, altura, largura*altura))
+print ('para pintar essa parede, você precisa de {}L de tinta'.format (área/2))
