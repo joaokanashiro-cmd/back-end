982d8b42ec9677411d1bb7001c4e98d9f522f66b
joaokanashiro-cmd <joao.kanashiro@escola.pr.gov.br>
2026-05-18 10:31:12 -0300

    Update ex.32.py

--- exercícios/ex.32.py
+++ exercícios/ex.32.py
@@ -1,5 +1,9 @@
-ano = int(input("Digite um ano para verificar: "))
-if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print(f"O ano {ano} é BISSEXTO.")
+from datetime import date
+ano = int(input("Que ano quer analisar? Coloque para analisar o ano atual"))
+if ano == 0:
+    ano = date.today().year
+bissexto = ano % 4
+if bissexto == 0 and ano%100 != 0 or ano% 400 == 0:
+    print("o ano {} é bissexto".format(ano))
 else:
-    print(f"O ano {ano} NÃO é bissexto.")
+    print("o ano {} não é bissexto".format(ano))
