227749c85002006dbeae63fbb3d352336da8dded
joaokanashiro-cmd <joao.kanashiro@escola.pr.gov.br>
2026-05-18 10:55:38 -0300

    Update ex.33.py

--- exercícios/ex.33.py
+++ exercícios/ex.33.py
@@ -4,9 +4,9 @@ n3 = float(input("Digite o terceiro número "))
 menor = n1
 maior = n1
 if n2 > n1 and n2>n3: 
-    maior = n2
+    maior = n3
 if n3 > n1 and n3>n2:
-    maior = n2
+    maior = n3
 if n2 < n1 and n2<n3:
     menor = n2
 if n3 < n1 and n3<n2:
